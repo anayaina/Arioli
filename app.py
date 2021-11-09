3c6e45e142941fbb9843f6f45755a621001a5c7e
Ana Anaya <anaya.anap@gmail.com>
2021-11-09 13:56:05 -0700

    regsitro
    Co-Authored-By: MarianaSilvaC <79737436+MarianaSilvaC@users.noreply.github.com>

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('principal.html')
 
+@app.route('/nosotros')
+def nosotros():
+    return render_template('nosotros.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
