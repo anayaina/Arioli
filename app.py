ede8e02a2e008516c79dfa43abded49540558631
Ana Anaya <anaya.anap@gmail.com>
2021-11-10 12:31:04 -0700

    image

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def principal():
     return render_template('principal.html')
 
 @app.route('/nosotros')
