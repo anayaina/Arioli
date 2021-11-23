60ad765d7ec4c2613b327d9b6df72bffd1288236
Mariana Silva <marscamargo123@gmail.com>
2021-11-23 12:03:30 -0700

    comidas

--- app.py
+++ app.py
@@ -34,6 +34,66 @@ def login():
 def registro():
     return render_template('registro.html')
 
+@app.route('/italiana')
+def italiana():
+    return render_template('italiana.html')
+
+@app.route('/argentina')
+def argentina():
+    return render_template('argentina.html')
+
+@app.route('/china')
+def china():
+    return render_template('china.html')
+
+@app.route('/cocina')
+def cocina():
+    return render_template('cocina.html')
+
+@app.route('/conservas')
+def conservas():
+    return render_template('conservas.html')
+
+@app.route('/japonesa')
+def japonesa():
+    return render_template('japonesa.html')
+
+@app.route('/tailandesa')
+def tailandesa():
+    return render_template('tailandesa.html')
+
+@app.route('/panaderia')
+def panaderia():
+    return render_template('panaderia.html')
+
+@app.route('/reposteria')
+def reposteria():
+    return render_template('reposteria.html')
+
+@app.route('/taquiza')
+def taquiza():
+    return render_template('taquiza.html')
+
+@app.route('/navidad')
+def navidad():
+    return render_template('navidad.html')
+
+@app.route('/parrilladas')
+def parilladas():
+    return render_template('parrilladas.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
