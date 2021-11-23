bae126e59d1c8533961f0da09fcb9d8014271073
Ana Anaya <anaya.anap@gmail.com>
2021-11-23 12:20:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,107 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def principal():
     return render_template('principal.html')
 
+
 @app.route('/nosotros')
 def nosotros():
     return render_template('nosotros.html')
+
 
 @app.route('/internacional')
 def internacional():
     return render_template('internacional.html')
 
+
 @app.route('/gourmet')
 def gourmet():
     return render_template('gourmet.html')
+
 
 @app.route('/especial')
 def especial():
     return render_template('especial.html')
 
+
 @app.route('/mexicana')
 def mexicana():
     return render_template('mexicana.html')
+
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
+
 @app.route('/registro')
 def registro():
     return render_template('registro.html')
 
-@app.route('/user')
-def user():
-    return render_template('user.html')
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
+@app.route('/italiana')
+def italiana():
+    return render_template('italiana.html')
+
+
+@app.route('/argentina')
+def argentina():
+    return render_template('argentina.html')
+
+
+@app.route('/china')
+def china():
+    return render_template('china.html')
+
+
+@app.route('/cocina')
+def cocina():
+    return render_template('cocina.html')
+
+
+@app.route('/conservas')
+def conservas():
+    return render_template('conservas.html')
+
+
+@app.route('/japonesa')
+def japonesa():
+    return render_template('japonesa.html')
+
+
+@app.route('/tailandesa')
+def tailandesa():
+    return render_template('tailandesa.html')
+
+
+@app.route('/panaderia')
+def panaderia():
+    return render_template('panaderia.html')
+
+
+@app.route('/reposteria')
+def reposteria():
+    return render_template('reposteria.html')
+
+
+@app.route('/taquiza')
+def taquiza():
+    return render_template('taquiza.html')
+
+
+@app.route('/navidad')
+def navidad():
+    return render_template('navidad.html')
+
+
+@app.route('/parrilladas')
+def parilladas():
+    return render_template('parrilladas.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
