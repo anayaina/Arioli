0458411b059b08df639c729731a994d031f4aa3d
Ana Anaya <anaya.anap@gmail.com>
2021-11-30 15:39:31 -0700

    tabla

--- static/js/admin.js
+++ static/js/admin.js
@@ -2,9 +2,88 @@ class DataTable {
     element;
     headers;
     items;
-    copyitems;
-    selectd;
-    paginations;
-    nimberofentres;
-    headerbuttons;
+    copyItems;
+    selected;
+    pagination;
+    numberOfEntries;
+    headerButtons;
+
+    constructor(selector, headerButtons) {
+        this.element = document.querySelector(selector);
+
+        this.headers = [];
+        this.items = [];
+        this.pagination = {
+            total: 0,
+            noItemsPerPage: 0,
+            noPages: 0,
+            actual: 0,
+            pointer: 0,
+            diff: 0,
+            lastpagebeforedots: 0,
+            nobuttonsbeforedots: 4
+        }
+        this.selected = [];
+        this.numberentries = [];
+        this.headerButtons = headerButtons;
+    }
+
+    parse() {
+        const headers = [...this.element.querySelector('thead tr').children];
+        const trs = [...this.element.querySelector('tbody').children];
+
+        headers.forEach(element => {
+            this.headers.push(element.textContent);
+        });
+
+        trs.forEach(tr => {
+            const cells = [...tr.children];
+            const item = {
+                id: this.generateUUID(),
+                values: []
+            };
+            cells.forEach(cell => {
+                if (cell.children.length > 0) {
+                    const status = [...cell.children][0].getAttribute('class');
+                    if (status != null) {
+                        item.values.push(`<span class='${status}'></span>`);
+                    }
+                } else {
+                    item.values.push(cell.textContent);
+                }
+            });
+            this.items.push(item);
+        });
+        console.log(this.items);
+
+        this.makeTable();
+    }
+
+    makeTable() {
+        this.copyItems = [...this.items];
+        this.initpagination(this.items.length, this.numberOfEntries);
+
+        const container = docuemnt.createElement('div');
+        container.id = this.element.id;
+        this.element.innerHTML = '';
+        this.element.replaceWith(container);
+
+        this.createHTML();
+        this.renderHeaders();
+        this.renderRows();
+        this.renderPagesButtons();
+    }
+
+    initPagination(total, entries) {
+        this.pagination, total = total;
+        this.pagination.noItemsPerPage = entries;
+        this.pagination.noPages = Math.ceil(this.pagination.total / this.pagination.noItemsPerPage);
+        this.pagination.actual = 1;
+        this.pagination.pointer = 0;
+        this.pagination.diff = this.paginations.noItemsPerPage - (this.pagination.total % this.pagination.noItemsPerPage);
+    }
+
+    generateUUID() {
+        return (Date.now() * Math.floor(Math.random() * 100000)).toString();
+    }
 }
