e2abd41e6518752ebf3159062e9ed39e7c05cf1c
Ana Anaya <anaya.anap@gmail.com>
2021-11-30 19:22:18 -0700

    Update admin.js

--- static/js/admin.js
+++ static/js/admin.js
@@ -44,11 +44,49 @@ class DataTable {
             };
             cells.forEach(cell => {
                 if (cell.children.length > 0) {
-
+                    //const status = [...cell.children][0].getAttribute('class');
+                    const statusElement = [...cell.children][0];
+                    const status = statusElement.getAttribute('class');
+                    if (status !== null) {
+                        item.values.push(`<span class='${status}'></span>`);
+                    }
+                } else {
+                    item.values.push(cell.textContent);
                 }
             });
+            this.items.push(item);
         });
+        console.log(this.items);
+
+        this.makeTable();
     }
+
+    makeTable() {
+        this.copyItems = [...this.items];
+
+        this.initPagination(this.item.lengt, this.numberOfEntries);
+
+        const container = document.createElement('div');
+        container.id = this.element.id;
+        this.element.innerHTML = '';
+        this.element.replaceWith(container);
+
+
+        this.createHTML();
+        this.renderHeaders();
+        this.rendererRows();
+        this.renderPagesButtons();
+    }
+
+    initPagination(total, entries) {
+        this.pagination.total = total;
+        this.pagination.noItemsPerPage = entries.length;
+        this.pagination.noPages = Math.ceil(this.pagination.total / this.pagination.noItemsPerPage);
+        this.pagination.actual = 1;
+        this.pagination.pointer = 0;
+        this.pagination.diff = this.pagination.noItemsPerPage - (this.pagination.total % this.pagination.noItemsPerPage);
+    }
+
     generateUUID() {
         return (Date.now() * Math.floor(Math.random() * 1000000)).toString;
     }
