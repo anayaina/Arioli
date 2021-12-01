d65c935a93ea7eabae71fd10f4b14969c37862db
Ana Anaya <anaya.anap@gmail.com>
2021-11-30 23:06:25 -0700

    upsi

--- static/js/admin.js
+++ static/js/admin.js
@@ -13,9 +13,10 @@ function onFormSubmit() {
 
 function readFormData() {
     var formData = {};
-    formData["producto"] = document.getElementById("producto").value;
-    formData["insumo"] = document.getElementById("insumo").value;
-    formData["prov"] = document.getElementById("prov").value;
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["empCode"] = document.getElementById("empCode").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["city"] = document.getElementById("city").value;
     return formData;
 }
 
@@ -29,22 +30,26 @@ function insertNewRecord(data) {
     cell3 = newRow.insertCell(2);
     cell3.innerHTML = data.salary;
     cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.city;
+    cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
 }
 
 function resetForm() {
-    document.getElementById("producto").value = "";
-    document.getElementById("insumo").value = "";
-    document.getElementById("prov").value = "";
+    document.getElementById("fullName").value = "";
+    document.getElementById("empCode").value = "";
+    document.getElementById("salary").value = "";
+    document.getElementById("city").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("producto").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("insumo").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("prov").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
 }
 
 function updateRecord(formData) {
@@ -55,7 +60,7 @@ function updateRecord(formData) {
 }
 
 function onDelete(td) {
-    if (confirm('Estas seguro que quieres eliminar?')) {
+    if (confirm('Are you sure to delete this record ?')) {
         row = td.parentElement.parentElement;
         document.getElementById("employeeList").deleteRow(row.rowIndex);
         resetForm();
@@ -64,7 +69,7 @@ function onDelete(td) {
 
 function validate() {
     isValid = true;
-    if (document.getElementById("producto").value == "") {
+    if (document.getElementById("fullName").value == "") {
         isValid = false;
         document.getElementById("fullNameValidationError").classList.remove("hide");
     } else {
