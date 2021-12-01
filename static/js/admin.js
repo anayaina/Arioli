dd377ef961715ccaaa055e48b1cb28c0b603a977
Ana Anaya <anaya.anap@gmail.com>
2021-11-30 23:19:51 -0700

    campo obligatorio

--- static/js/admin.js
+++ static/js/admin.js
@@ -13,69 +13,69 @@ function onFormSubmit() {
 
 function readFormData() {
     var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["empCode"] = document.getElementById("empCode").value;
-    formData["salary"] = document.getElementById("salary").value;
-    formData["city"] = document.getElementById("city").value;
+    formData["producto"] = document.getElementById("producto").value;
+    formData["insumo"] = document.getElementById("insumo").value;
+    formData["prov"] = document.getElementById("prov").value;
     return formData;
 }
 
 function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var table = document.getElementById("tablaproducto").getElementsByTagName('tbody')[0];
     var newRow = table.insertRow(table.length);
     cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
+    cell1.innerHTML = data.producto;
     cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.empCode;
+    cell2.innerHTML = data.insumo;
     cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.salary;
+    cell3.innerHTML = data.prov;
     cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.city;
-    cell4 = newRow.insertCell(4);
     cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
                        <a onClick="onDelete(this)">Delete</a>`;
 }
 
 function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("empCode").value = "";
-    document.getElementById("salary").value = "";
-    document.getElementById("city").value = "";
+    document.getElementById("producto").value = "";
+    document.getElementById("insumo").value = "";
+    document.getElementById("prov").value = "";
     selectedRow = null;
 }
 
 function onEdit(td) {
     selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("empCode").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("salary").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("producto").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("insumo").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("prov").value = selectedRow.cells[2].innerHTML;
 }
 
 function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.empCode;
-    selectedRow.cells[2].innerHTML = formData.salary;
-    selectedRow.cells[3].innerHTML = formData.city;
+    selectedRow.cells[0].innerHTML = formData.producto;
+    selectedRow.cells[1].innerHTML = formData.producto;
+    selectedRow.cells[2].innerHTML = formData.prov;
 }
 
 function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
+    if (confirm('Estas seguro que quieres eliminar?')) {
         row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        document.getElementById("tablaproducto").deleteRow(row.rowIndex);
         resetForm();
     }
 }
 
 function validate() {
     isValid = true;
-    if (document.getElementById("fullName").value == "") {
+    if (document.getElementById("producto").value == "", document.getElementById("insumo"), document.getElementById("prov").value == "") {
         isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
+        document.getElementById("productoValidationError").classList.remove("hide");
+        document.getElementById("insumoValidationError").classList.remove("hide");
+        document.getElementById("provValidationError").classList.remove("hide");
     } else {
         isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
+        if (!document.getElementById("productoValidationError").classList.contains("hide"))
+            document.getElementById("productoValidationError").classList.add("hide");
+        if (!document.getElementById("insumoValidationError").classList.contains("hide"))
+            document.getElementById("insumoValidationError").classList.add("hide");
+        if (!document.getElementById("provValidationError").classList.contains("hide"))
+            document.getElementById("provValidationError").classList.add("hide");
     }
     return isValid;
 }
