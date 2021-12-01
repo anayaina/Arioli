d35812d3f56b5b19faead46e2ab14d19b0aeb809
Ramon Lopez <63748740+Ramonst18@users.noreply.github.com>
2021-11-30 23:42:39 -0700

    Correccion de error al agregar productos al carrito

--- app.py
+++ app.py
@@ -390,15 +390,17 @@ if __name__ == '__main__':
 def insertarProductoAlCarrito(carrito,productId,email):
     #verificamos si el carrito esta vacio
     if carrito is None or len(carrito) == 0:
-        carrito = dict()
-        carrito[productId] = [1, dict_productos[productId]]
+        carrito = list()
+        carrito.append(productId)
     else:  # si ya tiene elementos
-        if productId not in dict_usuarios:  # si aun no está el producto en el carrito
-            carrito[productId] = [1, dict_productos[productId]]
-        else:
-            carrito[productId][0] += 1
+        print(productId)
+        carrito.append(productId)
+        #if productId not in carrito:  # si aun no está el producto en el carrito
+        #    carrito[productId] = [1, dict_productos[productId]]
+        #else:
+        #    carrito[productId][0] += 1
 
-        dict_usuarios[email]["carrito"] = carrito
+    dict_usuarios[email]["carrito"] = carrito
 
     #realizamos la escritura
     with open('static/usuarios/usuarios.json', 'w') as fp:
